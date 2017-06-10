d81f5783d2dd56266a92e1e2c05ce4f13466af40
Sokratis Fotkatzikis <sokratis12GR@gmail.com>
2017-06-10 14:42:49 +0300

    Added a check to ensure logs when disabling recipes are debug mode only

--- src/main/java/net/thedragonteam/cct/events/ServerEventsHandler.java
+++ src/main/java/net/thedragonteam/cct/events/ServerEventsHandler.java
@@ -8,8 +8,7 @@ import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.thedragonteam.thedragonlib.util.LogHelper;
 
-import static net.thedragonteam.cct.CCTConfig.disableCCT;
-import static net.thedragonteam.cct.CCTConfig.disableVanilla;
+import static net.thedragonteam.cct.CCTConfig.*;
 import static net.thedragonteam.cct.registry.ModBlocks.blockCCT;
 
 public class ServerEventsHandler {
@@ -22,12 +21,12 @@ public class ServerEventsHandler {
             for (int i = 0; i < disableCCT.length; i++) {
                 if (disableCCT[i] && block == blockCCT[i]) {
                     e.setCanceled(true);
-                    LogHelper.INSTANCE.info("Denied access to: '" + block.getRegistryName() + "'.");
+                    if (enableDebug) LogHelper.INSTANCE.info("Denied access to: '" + block.getRegistryName() + "'.");
                 }
             }
             if (disableVanilla && block == Blocks.CRAFTING_TABLE) {
                 e.setCanceled(true);
-                LogHelper.INSTANCE.info("Denied access to: '" + block.getRegistryName() + "'.");
+                if (enableDebug) LogHelper.INSTANCE.info("Denied access to: '" + block.getRegistryName() + "'.");
             }
         }
     }
